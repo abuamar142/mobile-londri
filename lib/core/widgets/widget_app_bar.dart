b114ac0ef14ff28e7d97b68903905a981d073179
abuamar.albadawi <abuamar.albadawi@gmail.com>
2025-05-24 07:10:54 +0700

    [DEVELOPMENT] : Add property leading in widget app bar

--- lib/core/widgets/widget_app_bar.dart
+++ lib/core/widgets/widget_app_bar.dart
@@ -5,16 +5,19 @@ import '../../config/textstyle/app_textstyle.dart';
 class WidgetAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String label;
   final IconButton? action;
+  final Widget? leading;
 
   const WidgetAppBar({
     super.key,
     required this.label,
     this.action,
+    this.leading,
   });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: leading,
       title: Text(
         label,
         style: AppTextStyle.heading3,
